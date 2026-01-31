d2178d87f426ad9ca4dc14867b99f11708e00dc1
Abdul samad <bsse23014@itu.edu.pk>
2026-01-31 11:35:27 +0500

    SaveCart in local storage

--- JavaScriptProject/data/cart.js
+++ JavaScriptProject/data/cart.js
@@ -1,48 +1,26 @@
-import { jsxs } from "react/jsx-runtime";
+export let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-export let cart=JSON.parse(localStorage.getItem("cart"));
-
-if(!cart){
-    cart=[{
-    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity:2
-},{
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:1
-}
-];
+if (cart.length === 0) {
+  cart = [
+    { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 },
+    { productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d", quantity: 1 }
+  ];
+  saveToStorage();
 }
 
-// function to add in the card 
-export function addToCart(productId){
-   let match=0;
-    cart.forEach((item) => {
- if(productId==item.productId){
-   match = item;
- }
- });
- if(match){
-  match.quantity +=1;
- }else{
-  cart.push({
-     productId:productId,
-     quantity : 1
-    });
- }
- saveToStorage();
+// addToCart same idea, but clean up match variable
+export function addToCart(productId) {
+  const match = cart.find(item => item.productId === productId);
+  if (match) match.quantity += 1;
+  else cart.push({ productId, quantity: 1 });
+  saveToStorage();
 }
-// remove from cart 
-export function removeFromCart(pid){
-    const newCart=[];
- cart.forEach((cartItem)=>{
-    if(cartItem.productId!==pid){
-        newCart.push(cartItem)
-    }
- })
- cart=newCart;
- saveToStorage();
+
+export function removeFromCart(pid) {
+  cart = cart.filter(item => item.productId !== pid);
+  saveToStorage();
 }
-// save to locl Storage 
-function saveToStorage(){
-    localStorage.setItem("cart",JSON.stringify(cart));
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
